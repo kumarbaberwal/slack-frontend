df0f5fa30cdd8869ac12a259a0ca156380fcdd1c
kumarbaberwal <itsnitesh16@gmail.com>
2025-08-28 14:09:17 +0530

    call page implemented

--- src/pages/CallPage.jsx
+++ src/pages/CallPage.jsx
@@ -1,8 +1,97 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import "@stream-io/video-react-sdk/dist/css/styles.css"
+import { useNavigate, useParams, } from "react-router";
+import { useUser } from "@clerk/clerk-react";
+import { useQuery } from "@tanstack/react-query";
+import { getStreamToken } from "../lib/api";
+import { CallControls, CallingState, SpeakerLayout, StreamCall, StreamTheme, StreamVideo, StreamVideoClient, useCallStateHooks } from "@stream-io/video-react-sdk";
+import toast from "react-hot-toast";
+
+
+const STREAM_API_KEY = import.meta.env.VITE_STREAM_API_KEY;
 
 const CallPage = () => {
+  const { id: callId } = useParams();
+  const { user, isLoaded } = useUser();
+  const [client, setClient] = useState(null)
+  const [call, setCall] = useState(null)
+  const [isConnecting, setIsConnecting] = useState(true)
+
+  const { data: tokenData } = useQuery({
+    queryKey: ['streamToken'],
+    queryFn: getStreamToken,
+    enabled: !!user
+  });
+
+  useEffect(() => {
+    const initCall = async () => {
+      if (!tokenData.token || !user || !callId) return;
+
+      try {
+        const videoClient = new StreamVideoClient({
+          apiKey: STREAM_API_KEY,
+          user: {
+            id: user.id,
+            name: user.fullName,
+            image: user.image
+          },
+          token: tokenData.token
+        })
+
+        const callInstance = videoClient.call("default", callId);
+        await callInstance.join({ create: true });
+
+        setClient(videoClient);
+        setCall(callInstance);
+      } catch (error) {
+        console.error("Error initializing call:", error);
+        toast.error("Failed to join call");
+      } finally {
+        setIsConnecting(false);
+      }
+    }
+
+
+    initCall();
+  }, [tokenData, user, callId])
+
+  if (isConnecting || !isLoaded) return <div className="h-screen flex justify-center items-center">Connecting to call...</div>;
+
   return (
-    <div>CallPage</div>
+    <div className="h-screen flex flex-col items-center justify-center bg-gray-100">
+      <div className="relative w-full max-w-4xl mx-auto">
+        {client && call ? (
+          <StreamVideo client={client}>
+            <StreamCall call={call} >
+              <CallContent />
+            </StreamCall>
+          </StreamVideo>
+        ) : (
+          <div className="flex items-center justify-center h-full">
+            <p>
+              Could not join the call. Please check the call link or try again later.
+            </p>
+          </div>
+        )}
+      </div>
+
+    </div>
+  )
+}
+
+
+const CallContent = () => {
+  const { useCallCallingState } = useCallStateHooks()
+  const callingState = useCallCallingState();
+  const navigate = useNavigate();
+
+  if (callingState === CallingState.LEFT) return navigate('/');
+
+  return (
+    <StreamTheme>
+      <SpeakerLayout />
+      <CallControls />
+    </StreamTheme>
   )
 }
 
